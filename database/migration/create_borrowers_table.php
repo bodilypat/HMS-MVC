960d2efa84e6acc374074be108def1b6fa41195d
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-13 13:00:23 -0700

    Add files via upload

---
+++ database/migration/create_borrowers_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBorrowersTable extends Migration 
+{
+
+    /* Run the migrations. */
+    public function up()
+    {
+        Schema::create('borrows', function (Blueprint $table){
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /* Reverse the migrations */
+    public function down()
+    {
+        Schema::dropIfExists('borrowers');
+    }
+}
