9525eb8b580477458bbe79313d9f782eec271df7
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-13 12:42:42 -0700

    Add files via upload

---
+++ app/Models/Borrow.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Borrow extends Model 
+{
+    use HasFactory;
+
+    /* The attributes that are mass assignable */
+    protected $fileable = [
+        'book_id',
+        'user_id',
+        'borrowed_at',
+        'returned_at',
+    ];
+
+    /* Define the relationship with the Book model */
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
+
+    /* Define the relationship with the user model */
+    public function user()
+    {
+        return $this->belongsTo(user::class);
+    }
+}
