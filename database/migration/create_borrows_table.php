3fad813dd516555e93986d573a0a0bf55672de80
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-13 11:17:09 -0700

    Add files via upload

---
+++ database/migration/create_borrows_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBorrowsTable extends Migration
+{
+    /* Run the Migrations */
+    public function up()
+    {
+        Schema::create('borrows', function(Blueprint $table){ 
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('book_id')->constrained()->onDelete('cascade');
+            $table->timestamp('borrowed_at');
+            $table->timestamp('returned_at');->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('borrows');
+    }
+
+}
