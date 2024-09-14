30f88f25dce571069a7c12f8721e0d74270989a8
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-14 09:56:58 -0700

    Create index.blade.php

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Categories</title>
+    </head>
+    <body>
+        <h1>Categories</h1>
+        <a href="{{ route('categoires.create') }}">Add New Categories</a>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($categories as $category)
+                <tr>
+                    <td>{{ $category->name }}</td>
+                    <td>
+                        <a href="{{ route('categories.edit', $category->id) }}">Edit</a>
+                        <form action="{{ route('categories.destroy', $category->id) }}" method="POST" style="display:inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </body>
+</html>
