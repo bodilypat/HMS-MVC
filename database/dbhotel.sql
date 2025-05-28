4123a66486eed9a51d4fea6cf6fa2f4d6916da99
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-05-28 11:23:18 -0700

    Add files via upload

--- database/dbhotel.sql
+++ database/dbhotel.sql
@@ -10,78 +10,117 @@ CREATE TABLE users (
     ceate_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+/* Guests table */
 CREATE TABLE guests (
-    guest_id INT AUTO_INCREMENT PRIMARY KEY,          -- Unique ID for each guest
-    first_name VARCHAR(100) NOT NULL,                 -- Corrected spelling of 'first_name'
+    guest_id INT AUTO_INCREMENT PRIMARY KEY,          					-- Unique ID for each guest
+    first_name VARCHAR(100) NOT NULL,                 					-- Corrected spelling of 'first_name'
     last_name VARCHAR(100) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,               -- Added email data type and uniqueness constraint
-    phone_number VARCHAR(15),                         -- Adjusted to phone number format (can change the size if needed)
-    address TEXT,                                     -- Text for storing guest address, can be longer than VARCHAR
-    id_type ENUM('Passport', 'ID card') NOT NULL,     -- Enum for id_type to restrict values
-    id_number VARCHAR(50) NOT NULL,                   -- id_number should store the actual ID or passport number
-    dob DATE NOT NULL,                                -- Date of Birth (DOB)
-    nationality VARCHAR(50),                          -- Nationality can be a string
-    check_in_date DATE,                               -- Check-in date
-    check_out_date DATE,                              -- Check-out date
-    CONSTRAINT chk_dates CHECK (check_in_date <= check_out_date)  -- Ensure check-out date is after check-in date
+    email VARCHAR(255) NOT NULL UNIQUE,               					-- Added email data type and uniqueness constraint
+    phone_number VARCHAR(15),                         					-- Adjusted to phone number format (can change the size if needed)
+    address TEXT,                                     					-- Text for storing guest address, can be longer than VARCHAR
+    id_type ENUM('Passport', 'National ID','Driver Lincence') NOT NULL, -- Enum for id_type to restrict values
+    id_number VARCHAR(50) NOT NULL,                  					-- id_number should store the actual ID or passport number
+    dob DATE NOT NULL,                                					-- Date of Birth (DOB)
+    nationality VARCHAR(50) DEFAULT 'Unknow',,                          -- Nationality can be a string
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,                     -- Check-in date
+    updated_at TIMESTAMP CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  -- Check-out date
 );
 
-CREATE bookings (
-    booking_id INT AUTO_INCREMENT PRIMARY KEY,
-    guest_id INT NOT NULL,
-    room_id INT NOT NULL,
-    check_in DATE NOT NULL,
-    check_out DATE NOT NULL,
-    number_of_guests INT NOT NULL,
-    booking_status ENUM('Pending','Confirmed','Checked-in','Checked-Out','Cancelled') DEFAULT 'Pending',
-    payment_status ENUM('Pending','Paid') DEFAULT 'Pending',
-    booking_source ENUM('Website','Phone','Walk-in','Travel Agency','OTA') DEFAULT 'Website',
-    special_request TEXT,
-    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    FOREIGN KEY (guest_id) REFERENCES guests(guest_id) ON DELETE CASCADE,
-    FOREIGN KEY (room_id) REFERENCES rooms(room_id) ON DELETE CASCADE,
+/* Room_types table  */
+CREATE TABLE room_types (
+    room_type_id INT AUTO_INCREMENT PRIMARY KEY,
+    type_name VARCHAR(50) UNIQUE NOT NULL, -- e.g., Single, Double
+    description TEXT,
+    base_price DECIMAL(10, 2) NOT NULL CHECK (base_price >= 0)
+	default_capacity INT NOT NULL DEFAULT 1 CHECK (default_capacity > 0),
+	bed_count INT NOT NULL DEFAULT 1 CHECK (bed_count > 0),
+	amenities TEXT,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT TIMESTAMP
 );
 
+/* Rooms table */
 CREATE TABLE rooms (
     room_id INT AUTO_INCREMENT PRIMARY KEY,                    
     room_number VARCHAR(10) NOT NULL,                            
-    room_type ENUM('Single', 'Double', 'Suite', 'Other') NOT NULL, 
-    floor_number INT NOT NULL,                                   
-    price_per_night DECIMAL(10, 2) NOT NULL,                     
-    room_status ENUM('Available', 'Occupied', 'Maintenance') NOT NULL, 
+    room_type_id INT NOT NULL,  
+    floor_number INT NOT NULL CHECK (floor_number >= 0),                                   
+    price_per_night DECIMAL(10, 2) NOT NULL CHECK (price_per_night >= 0),                     
+    room_status ENUM('Available', 'Occupied', 'Maintenance') NOT NULL DEFAULT 'Available', 
     room_description TEXT,                                       
-    beds_count INT NOT NULL,                                     
-    capacity INT NOT NULL,                                       
-    CONSTRAINT chk_beds_capacity CHECK (beds_count <= capacity)  
+    beds_count INT NOT NULL CHECK (beds_count),                                     
+    capacity INT NOT NULL CHECK (capacity >= beds_count),                                       
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (room_type_id) REFERENCES room_types(room_types_id) ON DELETE RESTRICT 
 );
 
+/* Reservation table */
 CREATE TABLE reservations (
-    reservation_id INT AUTO_INCREMENT PRIMARY KEY,                  -- Unique reservation ID
-    guest_id INT NOT NULL,                                           -- Foreign key referencing Guests.guest_id
-    room_id INT NOT NULL,                                            -- Foreign key referencing Rooms.room_id
-    check_in DATE NOT NULL,                                          -- Check-in date
-    check_out DATE NOT NULL,                                         -- Check-out date
-    reservation_status ENUM('Confirmed', 'Cancelled', 'Pending') NOT NULL,  -- ENUM for reservation status
-    payment_status ENUM('Paid', 'Pending') NOT NULL,                 -- ENUM for payment status
-    CONSTRAINT fk_guest FOREIGN KEY (guest_id) REFERENCES guests(guest_id) ON DELETE CASCADE,  -- Foreign key constraint for guest_id
-    CONSTRAINT fk_room FOREIGN KEY (room_id) REFERENCES rooms(room_id) ON DELETE SET NULL,  -- Foreign key constraint for room_id
-    CONSTRAINT chk_dates CHECK (check_in <= check_out)               -- Check constraint to ensure check-in date is before check-out date
+    reservation_id INT AUTO_INCREMENT PRIMARY KEY,                  
+    guest_id INT NOT NULL,                                           
+    room_id INT NOT NULL,                                           
+    check_in DATE NOT NULL,                                          
+    check_out DATE NOT NULL,
+	number_of_guests INT NOT NULL DEFAULT 1 CHECK (number_of_checks > 0),
+    reservation_status ENUM('Pending','Confirmed','Checked-in','Checked-out','Cancelled') NOT NULL DEFAULT 'Pending',  
+    payment_status ENUM('Pending','Paid','Partially Paid','Refunded') NOT NULL DEFAULT 'Pending',
+	booking_source ENUM('Website','Phone','Walk-in','Traval Agency','OTA') NOT NULL 'Website',
+	special_request TEXT,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT chk_date (check_in <= check_out),   
+    CONSTRAINT fk_quest FOREIGN KEY (quest_id) REFERENCES guests(guest_id) ON DELETE CASCADE,  
+    CONSTRAINT fk_room FOREIGN KEY(room_id) REFERENCES rooms(room_id) ON DELETE SET NULL               
 );
 
-
-
+/* Payments table */
 CREATE TABLE payments (
-    payment_id INT AUTO_INCREMENT PRIMARY KEY,                              -- Unique payment ID
-    reservation_id INT NOT NULL,                                              -- Foreign key referencing Reservations.reservation_id
-    amount_paid DECIMAL(10, 2) NOT NULL,                                      -- Amount paid (with two decimal places)
-    payment_date DATETIME NOT NULL,                                           -- Date and time of the payment
-    payment_method ENUM('Credit Card', 'Cash', 'Online Transfer', 'Other') NOT NULL, -- ENUM for payment method
-    payment_status ENUM('Completed', 'Pending', 'Failed') NOT NULL,           -- ENUM for payment status
-    CONSTRAINT fk_reservation FOREIGN KEY (reservation_id) REFERENCES reservations(reservation_id) ON DELETE CASCADE,  -- Foreign key constraint for reservation_id
-    CONSTRAINT chk_amount_paid CHECK (amount_paid >= 0)                       -- Check constraint to ensure the amount paid is not negative
+    payment_id INT AUTO_INCREMENT PRIMARY KEY,                              
+    reservation_id INT NOT NULL,                                              
+    amount_paid DECIMAL(10, 2) NOT NULL CHECK (amount_paid >= 0),
+	currency 
+    payment_date DATETIME NOT NULL,                                          
+    payment_method ENUM('Credit Card', 'Cash', 'Online Transfer', 'Other') NOT NULL, 
+    payment_status ENUM('Completed', 'Pending', 'Failed') NOT NULL,           
+    CONSTRAINT fk_reservation FOREIGN KEY (reservation_id) REFERENCES reservations(reservation_id) ON DELETE CASCADE,  
+    CONSTRAINT chk_amount_paid CHECK (amount_paid >= 0)                      
 );
 
+/* Billings table */
+CREATE TABLE billings (
+    billing_id INT AUTO_INCREMENT PRIMARY KEY,                              
+    reservation_id INT NOT NULL,                                             
+    service_charge DECIMAL(10, 2) DEFAULT 0.00,                             
+    discount DECIMAL(10, 2) DEFAULT 0.00,                                    
+    total_amount DECIMAL(10, 2) NOT NULL,                                   
+    payment_status ENUM('Paid', 'Unpaid') NOT NULL,                          
+    CONSTRAINT fk_reservation FOREIGN KEY (reservation_id) REFERENCES reservations(reservation_id) ON DELETE CASCADE, 
+    CONSTRAINT chk_total_amount CHECK (total_amount >= 0)                    
+);
 
+/* services table */
+CREATE TABLE services (
+    service_id INT AUTO_INCREMENT PRIMARY KEY,
+    service_type VARCHAR(100) NOT NULL,
+    description TEXT,
+    price DECIMAL(10, 2) NOT NULL
+);
+
+/* Room_services table */
+CREATE TABLE room_services (
+    room_service_id INT AUTO_INCREMENT PRIMARY KEY,
+    reservation_id INT NOT NULL,
+    service_id INT NOT NULL,
+    service_request_time DATETIME NOT NULL,
+    service_status ENUM('Requested', 'In Progress', 'Delivered') NOT NULL,
+    CONSTRAINT chk_service_request_time CHECK (service_request_time <= NOW()),
+    FOREIGN KEY (reservation_id) REFERENCES reservations(reservation_id) ON DELETE CASCADE,
+    FOREIGN KEY (service_id) REFERENCES services(service_id)
+);
+
+/* staff table */
 CREATE TABLE staffs (
     staff_id INT AUTO_INCREMENT PRIMARY KEY,                              -- Unique staff ID
     first_name VARCHAR(100) NOT NULL,                                       -- Staff first name
@@ -95,7 +134,7 @@ CREATE TABLE staffs (
     CONSTRAINT chk_salary CHECK (salary >= 0)                               -- Check to ensure salary is not negative
 );
 
-
+/* Housekeeping table */
 CREATE TABLE housekeepings (
     housekeeping_id INT AUTO_INCREMENT PRIMARY KEY,                                  -- Unique housekeeping record ID
     room_id INT NOT NULL,                                                           -- Foreign key referencing Rooms.room_id
@@ -107,29 +146,7 @@ CREATE TABLE housekeepings (
     CONSTRAINT chk_cleaning_date CHECK (cleaning_date <= NOW())                       -- Check constraint to ensure cleaning_date is not in the future
 );
 
-
-CREATE TABLE room_services (
-    room_service_id INT AUTO_INCREMENT PRIMARY KEY,                               -- Unique room service ID
-    reservation_id INT NOT NULL,                                                  -- Foreign key referencing Reservations.reservation_id
-    service_type ENUM('Food', 'Beverages', 'Other Requests') NOT NULL,             -- ENUM for service type
-    service_request_time DATETIME NOT NULL,                                       -- Date and time the service was requested
-    service_status ENUM('Requested', 'In Progress', 'Delivered') NOT NULL,        -- ENUM for service status
-    CONSTRAINT fk_reservation FOREIGN KEY (reservation_id) REFERENCES reservations(reservation_id) ON DELETE CASCADE,  -- Foreign key constraint for reservation_id
-    CONSTRAINT chk_service_request_time CHECK (service_request_time <= NOW())      -- Check constraint to ensure the service request time is not in the future
-);
-
-
-CREATE TABLE billings (
-    billing_id INT AUTO_INCREMENT PRIMARY KEY,                              -- Unique billing ID
-    reservation_id INT NOT NULL,                                             -- Foreign key referencing Reservations.reservation_id
-    service_charge DECIMAL(10, 2) DEFAULT 0.00,                              -- Service charge (decimal with two decimal places)
-    discount DECIMAL(10, 2) DEFAULT 0.00,                                    -- Discount (decimal with two decimal places)
-    total_amount DECIMAL(10, 2) NOT NULL,                                    -- Total amount with two decimal places
-    payment_status ENUM('Paid', 'Unpaid') NOT NULL,                          -- ENUM for payment status
-    CONSTRAINT fk_reservation FOREIGN KEY (reservation_id) REFERENCES reservations(reservation_id) ON DELETE CASCADE,  -- Foreign key constraint for reservation_id
-    CONSTRAINT chk_total_amount CHECK (total_amount >= 0)                    -- Check constraint to ensure total_amount is not negative
-);
-
+/* Feedback Table */
 CREATE TABLE feedbacks (
     feedback_id INT AUTO_INCREMENT PRIMARY KEY,                                  -- Unique feedback ID
     guest_id INT NOT NULL,                                                       -- Foreign key referencing Guests.guest_id
